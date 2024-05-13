78d06c2b48c5b211cff7642f19af34e5ce7198ec
luisa <moraLF20@outlook.com>
2024-05-13 00:39:25 -0700

    mas pruebas

--- PersistenciaJuatsapp/src/main/java/lum/pruebas/PersistenciaJuatsapp.java
+++ PersistenciaJuatsapp/src/main/java/lum/pruebas/PersistenciaJuatsapp.java
@@ -4,14 +4,21 @@
 
 package lum.pruebas;
 
+import daoColecciones.ChatDAO;
+import daoColecciones.MensajeDAO;
 import daoColecciones.UsuarioDAO;
 import encriptado.Encriptacion;
 import entidades.Direccion;
+import entidades.EntidadChat;
+import entidades.EntidadMensaje;
 import entidades.EntidadUsuario;
 import entidades.SexoEnum;
 import excepciones.PersistenciaException;
+import interfaces.IChatDAO;
+import interfaces.IMensajeDAO;
 import interfaces.IUsuarioDAO;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  *
@@ -33,26 +40,81 @@ public class PersistenciaJuatsapp {
 //            System.out.println(e);
 //        }
 //-------------------Buscar usuario-------------
-        EntidadUsuario usuario2=new EntidadUsuario();
-        usuario2.setTelefono("6441445522");
+//        EntidadUsuario usuario2=new EntidadUsuario();
+//        usuario2.setTelefono("6441445522");
+//        try {
+//            usuario2=dao.obtenerUsuario(usuario2);
+//            if(usuario2!=null)System.out.println(usuario2);
+//        } catch (PersistenciaException e) {
+//            System.out.println(e);
+//        }
+//------------------Probando cifrado---------------------
+//        interfaces.IEncriptacion cript=new Encriptacion();
+//        String password =usuario2.getContrasena();
+//        String hashedPassword = cript.genrarHash(password);
+//        System.out.println("Contraseña encriptada: " + hashedPassword);
+//
+//        // Ejemplo de verificación de contraseña
+//        String passwordToCheck = "1234";
+//        if (cript.verificarContra(passwordToCheck, hashedPassword)) {
+//            System.out.println("La contraseña es correcta");
+//        } else {
+//            System.out.println("La contraseña es incorrecta");
+//        }
+//-------------------Agregar chat-------------        
+        IChatDAO cDao=new ChatDAO();
+//        EntidadChat chat=new EntidadChat();
+//        EntidadUsuario user1=new EntidadUsuario();
+//        user1.setNombre("luisa");
+//        user1.setTelefono("6441445522");
+//        EntidadUsuario user2=new EntidadUsuario();
+//        user2.setNombre("usuario 2");
+//        user2.setTelefono("6442110022");
+//        chat.setContacto(user2);
+//        chat.setCreador(user1);
+//        try {
+//            chat=cDao.agregarChat(chat);
+//            System.out.println("chat: "+chat);
+//        } catch (PersistenciaException e) {
+//            System.out.println(e);
+//        }
+//-------------------Buscar chat-------------        
+        EntidadChat chat=new EntidadChat();
+        EntidadUsuario user1=new EntidadUsuario();
+        user1.setNombre("luisa");
+        user1.setTelefono("6441445522");
+        EntidadUsuario user2=new EntidadUsuario();
+        user2.setNombre("usuario 2");
+        user2.setTelefono("6442110022");
+        chat.setContacto(user2);
+        chat.setCreador(user1);
         try {
-            usuario2=dao.obtenerUsuario(usuario2);
-            if(usuario2!=null)System.out.println(usuario2);
+            chat=cDao.buscarChat(chat);
+            System.out.println("chat: "+chat);
         } catch (PersistenciaException e) {
             System.out.println(e);
         }
-//------------------Probando cifrado---------------------
-        interfaces.IEncriptacion cript=new Encriptacion();
-        String password =usuario2.getContrasena();
-        String hashedPassword = cript.genrarHash(password);
-        System.out.println("Contraseña encriptada: " + hashedPassword);
-
-        // Ejemplo de verificación de contraseña
-        String passwordToCheck = "1234";
-        if (cript.verificarContra(passwordToCheck, hashedPassword)) {
-            System.out.println("La contraseña es correcta");
-        } else {
-            System.out.println("La contraseña es incorrecta");
+//-------------------Agregar mensaje a chat------------- 
+//        EntidadMensaje mensaje=new EntidadMensaje();
+//        Calendar fecha=Calendar.getInstance();
+//        mensaje.setFechaHora(fecha.getTime());
+//        mensaje.setTexto("mensaje 1");
+//        mensaje.setRemitente(user2);
+//        try {
+//            chat=cDao.agregarMensaje(chat, mensaje);
+//            System.out.println("chat con mensaje: "+chat);
+//        } catch (PersistenciaException e) {
+//            System.out.println(e);
+//        }
+        //---------------buscar mensajes de un chat----------
+        IMensajeDAO mDao=new MensajeDAO();
+        try{
+            List<EntidadMensaje> mensajes=mDao.buscarMensajes(chat);
+            mensajes.forEach(m->{
+                System.out.println(m);
+            });
+        }catch(PersistenciaException e){
+            System.out.println(e);
         }
     }
 }
