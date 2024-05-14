0100d8aac394e123dbb8bae67002b35165fb4524
luisa <moraLF20@outlook.com>
2024-05-13 19:32:36 -0700

    modificando mensajeDAO
    le agregue un parametro al metodoBuscar mensajes que no tenia parametro y agregue la documentacion

--- PersistenciaJuatsapp/src/main/java/daoColecciones/MensajeDAO.java
+++ PersistenciaJuatsapp/src/main/java/daoColecciones/MensajeDAO.java
@@ -5,17 +5,26 @@
 package daoColecciones;
 
 import com.mongodb.MongoException;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Field;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.result.UpdateResult;
 import entidades.EntidadChat;
 import entidades.EntidadMensaje;
+import entidades.EntidadUsuario;
 import excepciones.PersistenciaException;
 import interfaces.IMensajeDAO;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import org.bson.conversions.Bson;
 
 /**
@@ -48,13 +57,31 @@ public class MensajeDAO implements IMensajeDAO{
     }
 
     @Override
-    public EntidadMensaje editarMensaje(EntidadMensaje mensaje) throws PersistenciaException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public EntidadMensaje editarMensaje(EntidadMensaje mensaje, EntidadMensaje mensajeNuevo) throws PersistenciaException {
+        try {
+            UpdateResult result=coleccion.updateOne(Filters.eq("_id", mensaje.getId()), 
+                    Updates.set("texto", mensajeNuevo.getTexto()));
+            if(result.getModifiedCount()>0){
+                mensaje.setTexto(mensajeNuevo.getTexto());
+                return mensaje;
+            }
+            LOG.log(Level.SEVERE,"no se edito el mensaje");
+            return null;
+        } catch (MongoException e) {
+            LOG.log(Level.SEVERE, e.getMessage(), e);
+            throw new PersistenciaException("Hubo un error al editar el mensaje");
+        }
     }
 
     @Override
-    public EntidadMensaje eliminarMensaje(EntidadMensaje mensaje) throws PersistenciaException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public boolean eliminarMensaje(EntidadMensaje mensaje) throws PersistenciaException {
+        try {
+            DeleteResult result=coleccion.deleteOne(Filters.eq("_id", mensaje.getId()));
+            return result.getDeletedCount()>0;
+        } catch (MongoException e) {
+            LOG.log(Level.SEVERE, e.getMessage(), e);
+            throw new PersistenciaException("Hubo un error al borrar el mensaje");
+        }
     }
 
     @Override
@@ -68,10 +95,12 @@ public class MensajeDAO implements IMensajeDAO{
     }
 
     @Override
-    public List<EntidadMensaje> buscarMensajes() throws PersistenciaException {
+    public List<EntidadMensaje> buscarMensajes(EntidadUsuario usuario) throws PersistenciaException {
         try {
             List<EntidadMensaje> mensajes=new ArrayList<>();
-            return coleccion.find().into(mensajes);
+            return coleccion.find(Filters.and(
+                    Filters.eq("remitente.nombre", usuario.getNombre()),
+                    Filters.eq("remitente.telefono", usuario.getTelefono()))).into(mensajes);
         } catch (MongoException e){
             LOG.log(Level.SEVERE, e.getMessage(), e);
             throw new PersistenciaException("Hubo un error al buscar los mensajes");
@@ -86,11 +115,6 @@ public class MensajeDAO implements IMensajeDAO{
                 mensajes.add(coleccion.find(Filters.eq("_id", i)).first());
             });
             return mensajes;
-//            Bson filtro = Filters.and(
-//                    Filters.eq("chat.creador.telefono", chat.getCreador().getTelefono()),
-//                    Filters.eq("chat.contacto.telefono", chat.getContacto().getTelefono())
-//            );
-//            return coleccion.find(filtro).into(mensajes);
         } catch (MongoException e){
             LOG.log(Level.SEVERE, e.getMessage(), e);
             throw new PersistenciaException("Hubo un error al buscar los mensajes");
@@ -98,8 +122,39 @@ public class MensajeDAO implements IMensajeDAO{
     }
 
     @Override
+    public List<EntidadMensaje> buscarMensajesFiltrados(EntidadChat chat,EntidadMensaje mensajeFiltrado) throws PersistenciaException {
+        try {
+            List<Bson> pipeline=new ArrayList<>();
+            Pattern pattern=Pattern.compile(mensajeFiltrado.getTexto(), Pattern.CASE_INSENSITIVE);
+            pipeline.add(Aggregates.match(Filters.regex("texto", pattern)));
+            pipeline.add(Aggregates.lookup("Chats", "_id", "mensajes._id", "chatJoin"));
+            pipeline.add(Aggregates.unwind("$chatJoin"));
+            pipeline.add(Aggregates.project( Projections.fields(
+                    Projections.include("texto","fechaHora","remitente.nombre"),
+                    Projections.computed("chat", "$chatJoin.contacto")
+            )
+            ));
+            AggregateIterable<EntidadMensaje> aggregate=coleccion.aggregate(pipeline);
+            if(aggregate.first()!=null){
+                List<EntidadMensaje> mensajes=new ArrayList<>();
+                aggregate.into(mensajes);
+                return mensajes;
+            }return null;
+        } catch (MongoException e){
+            LOG.log(Level.SEVERE, e.getMessage(), e);
+            throw new PersistenciaException("Hubo un error al buscar los mensajes");
+        }
+    }
+    
+    @Override
     public List<EntidadMensaje> buscarMensajesFiltrados(EntidadMensaje mensajeFiltrado) throws PersistenciaException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            List<EntidadMensaje> todosLosMensajes=new ArrayList<>();
+            return coleccion.find(Filters.eq("texto", mensajeFiltrado.getTexto())).into(todosLosMensajes);
+        } catch (MongoException e){
+            LOG.log(Level.SEVERE, e.getMessage(), e);
+            throw new PersistenciaException("Hubo un error al buscar los mensajes");
+        }
     }
     
 }
--- PersistenciaJuatsapp/src/main/java/interfaces/IMensajeDAO.java
+++ PersistenciaJuatsapp/src/main/java/interfaces/IMensajeDAO.java
@@ -6,6 +6,7 @@ package interfaces;
 
 import entidades.EntidadChat;
 import entidades.EntidadMensaje;
+import entidades.EntidadUsuario;
 import excepciones.PersistenciaException;
 import java.util.List;
 
@@ -14,11 +15,64 @@ import java.util.List;
  * @author LuisaM
  */
 public interface IMensajeDAO {
+    /**
+     * 
+     * @param mensaje
+     * @return
+     * @throws PersistenciaException 
+     */
     EntidadMensaje guardarMensaje(EntidadMensaje mensaje)throws PersistenciaException;
-    EntidadMensaje editarMensaje(EntidadMensaje mensaje)throws PersistenciaException;
-    EntidadMensaje eliminarMensaje(EntidadMensaje mensaje)throws PersistenciaException;
+    /**
+     * 
+     * @param mensaje
+     * @param mensajeNuevo
+     * @return
+     * @throws PersistenciaException 
+     */
+    EntidadMensaje editarMensaje(EntidadMensaje mensaje, EntidadMensaje mensajeNuevo)throws PersistenciaException;
+    /**
+     * 
+     * @param mensaje
+     * @return
+     * @throws PersistenciaException 
+     */
+    boolean eliminarMensaje(EntidadMensaje mensaje)throws PersistenciaException;
+    /**
+     * Busca un mensaje en especifico dentro de la coleccion de mensajes. 
+     * Usa el id del mensaje como criterio de busqueda
+     * @param mensaje Mensaje con el id a buscar en la coleccion
+     * @return La entidad mensaje encontrada, null si no se encontró
+     * @throws PersistenciaException En caso de que ocurra un error de base de datos
+     * en el proceso de busqueda
+     */
     EntidadMensaje buscarMensaje(EntidadMensaje mensaje)throws PersistenciaException;
-    List<EntidadMensaje> buscarMensajes()throws PersistenciaException;
+    /**
+     * Busca todos los mensajes de la coleccion donde el remitente 
+     * @param usuario
+     * @return
+     * @throws PersistenciaException 
+     */
+    List<EntidadMensaje> buscarMensajes(EntidadUsuario usuario)throws PersistenciaException;
+    /**
+     * 
+     * @param chat
+     * @return
+     * @throws PersistenciaException 
+     */
     List<EntidadMensaje> buscarMensajes(EntidadChat chat)throws PersistenciaException;
+    /**
+     * 
+     * @param chat
+     * @param mensajeFiltrado
+     * @return
+     * @throws PersistenciaException 
+     */
+    List<EntidadMensaje> buscarMensajesFiltrados(EntidadChat chat,EntidadMensaje mensajeFiltrado)throws PersistenciaException;
+    /**
+     * 
+     * @param mensajeFiltrado
+     * @return
+     * @throws PersistenciaException 
+     */
     List<EntidadMensaje> buscarMensajesFiltrados(EntidadMensaje mensajeFiltrado)throws PersistenciaException;
 }
