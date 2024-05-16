2b3178fdef8a85a1bc9c30790445de77f98b9581
luisa <moraLF20@outlook.com>
2024-05-15 23:36:48 -0700

    creando capa presentacion

---
+++ PresentacionJuatsapp/src/main/java/pantallas/Parent.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pantallas;
+
+import DTOs.UsuarioDTO;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import fachadas.FachadaControlUsuarios;
+
+/**
+ *
+ * @author LuisaM
+ */
+public class Parent extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+    protected static FachadaControlUsuarios fachadaUsuarios;
+    protected static UsuarioDTO user;
+    
+    public Parent(){
+        setTitle("Juatsapp");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(393, 700);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        
+        fachadaUsuarios=new FachadaControlUsuarios();
+        
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        
+        cardPanel.add(new LoginPanel(), "login");
+        cardPanel.add(new StartPanel(), "start");
+        cardPanel.add(new SignInPanel(), "signIn");
+        getContentPane().add(cardPanel, BorderLayout.CENTER);
+    }
+    
+    public void mostrarVentana(String nombrePanel) {
+        cardLayout.show(cardPanel, nombrePanel);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Parent parent = new Parent();
+                parent.setVisible(true);
+            }
+        });
+    }
+}
---
+++ PresentacionJuatsapp/src/main/java/pruebas/PresentacionJuatsapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pruebas;
+
+/**
+ *
+ * @author LuisaM
+ */
+public class PresentacionJuatsapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
